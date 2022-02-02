49f8427a6704eedd3c6dd5cb550c887bf547264a
viksor <dicvoy@gmail.com>
2022-02-02 11:40:02 +0000

    bcrypt added

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-Parser");
 const ejs  = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+
 
 const app = express();
 
@@ -21,7 +23,6 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -36,19 +37,23 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+
+  bcrypt.hash(req.body.password, salt, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err)
+      }else{
+        res.render("secrets");
+      }
+    });
+  });
   });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err)
-    }else{
-      res.render("secrets");
-    }
-  });
-});
+
 
 app.post("/login", function(req,res){
   const username = req.body.username;
@@ -59,14 +64,16 @@ app.post("/login", function(req,res){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true){
+            res.render("secrets");
+          }
+        });
         }
       }
-    }
+    });
   });
-});
 
 app.listen(3000, function(){
-  console.log("server started on port 3000");
+   console.log("server started on port 3000");
 });
